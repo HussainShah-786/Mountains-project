6fd158682e50bd3be05920dd79c6250ccbf2823b
HussainShah-786 <hussainshah58435@gmail.com>
2024-12-18 07:35:52 -0800

    slider changes updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import { FaYoutube } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdPhoneInTalk } from "react-icons/md";
 import { PiLineVerticalBold } from "react-icons/pi";
+import Button from "../Button/Button";
 
 const Header: React.FC = () => {
   return (
@@ -15,27 +16,29 @@ const Header: React.FC = () => {
             <div>
               <FaLocationDot />
             </div>
-            225 Private Road 4231 Clifton, TX 76634
+            <h1 className=" text-[10px] md:text-sm font-medium"> 225 Private Road 4231 Clifton, TX 76634</h1>
           </div>
           <div>
             <PiLineVerticalBold />
           </div>
           <div className="inline-flex items-center gap-2">
-            <MdPhoneInTalk /> 817-658-3961
+            <MdPhoneInTalk /> 
+            <h1 className=" text-[10px] md:text-sm font-medium"> 817-658-3961</h1>
           </div>
         </div>
-        <div className="flex gap-8 items-center">
+        <div className="flex gap-8 items-center max-sm:mt-2">
           <div className="flex gap-4 items-center ">
-            <div>Follow us on </div>
+            <div><h1 className=" text-[10px] md:text-sm font-medium"> Follow us Now</h1> </div>
             <div className="inline-flex gap-3">
-              <FaFacebookSquare className="w-7 h-7" />
-              <AiFillInstagram className="w-7 h-7" />
-              <FaYoutube className="w-7 h-7" />
+              <FaFacebookSquare className=" w-4 h-4 md:w-7 md:h-7" />
+              <AiFillInstagram className="w-4 h-4 md:w-7 md:h-7" />
+              <FaYoutube className="w-4 h-4 md:w-7 md:h-7" />
             </div>
           </div>
 
           <div>
-            <button className="border p-2 rounded-md">Contact us</button>
+            {/* <button className="border p-2    sm:w-32 sm:h-10 rounded-md ">Contact us</button> */}
+            <Button  className="border  p-0   w-[110px]  sm:h-10 h-8 rounded-md">Contact us</Button>
           </div>
         </div>
       </div>
